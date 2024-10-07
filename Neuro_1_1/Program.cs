a867bfdff10cc5af2bb20dd401b98198f7b5ef2a
latysh.ivan1122 <latysh.ivan1122@vu.cdu.edu.ua>
2024-10-07 20:12:16 +0300

    Добавьте файлы проекта.

---
+++ Neuro_1_1/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LinearClassifier
+{
+    public double[] coefficients;
+    public double bias;
+    private double learningRate;
+    private int maxEpochs;
+
+    public LinearClassifier(double[] initialCoefficients, double initialBias = -1.2, double learningRate = 0.15, int maxEpochs = 1000)
+    {
+        this.coefficients = initialCoefficients;
+        this.learningRate = learningRate;
+        this.bias = initialBias;
+        this.maxEpochs = maxEpochs;
+    }
+
+    public void Train(List<DataPoint> dataSet)
+    {
+        int iteration = 1;
+        bool hasMisclassified = true;
+        int epoch = 0;
+
+        while (hasMisclassified)
+        {
+            epoch++;
+            hasMisclassified = false;
+            foreach (var dataPoint in dataSet)
+            {
+                double output = ComputeOutput(dataPoint.x1, dataPoint.x2);
+
+                int actualLabel = dataPoint.label;
+                int predictedLabel = output >= 0 ? 1 : -1;
+
+                if (predictedLabel != actualLabel)
+                {
+                    coefficients[0] += learningRate * (actualLabel - predictedLabel) * dataPoint.x1;
+                    coefficients[1] += learningRate * (actualLabel - predictedLabel) * dataPoint.x2;
+                    bias += learningRate * (actualLabel - predictedLabel);
+
+                    hasMisclassified = true;
+                }
+
+                Console.WriteLine($"Ітерація {iteration}: ваги: w1 = {coefficients[0]:f3}, w2 = {coefficients[1]:f3}, bias = {bias:f3}");
+                iteration++;
+            }
+
+            if (epoch >= maxEpochs)
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine($"Фінальні ваги: w1 = {coefficients[0]}, w2 = {coefficients[1]}, bias = {bias}");
+    }
+
+    public double ComputeOutput(double x1, double x2)
+    {
+        return coefficients[0] * x1 + coefficients[1] * x2 + bias;
+    }
+}
+
+class DataPoint
+{
+    public double x1 { get; set; }
+    public double x2 { get; set; }
+    public int label { get; set; }
+
+    public DataPoint(double x1, double x2, int label)
+    {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.label = label;
+    }
+}
+
+class Program
+{
+    static List<DataPoint> CreateDataSet(List<DataPoint> existingDataSet)
+    {
+        List<DataPoint> dataSet;
+        if (existingDataSet == null)
+        {
+            dataSet = new List<DataPoint>();
+        }
+        else
+        {
+            dataSet = existingDataSet;
+        }
+
+        while (true)
+        {
+            Console.WriteLine("Введіть 'stop', щоб завершити введення:");
+
+            Console.Write("Введiть x1: ");
+            string inputX1 = Console.ReadLine();
+            if (inputX1.ToLower() == "stop")
+                break;
+
+            Console.Write("Введiть x2: ");
+            string inputX2 = Console.ReadLine();
+            if (inputX2.ToLower() == "stop")
+                break;
+
+            Console.Write("Введiть x3: ");
+            string inputLabel = Console.ReadLine();
+            if (inputLabel.ToLower() == "stop")
+                break;
+
+            if (double.TryParse(inputX1, out double x1) && double.TryParse(inputX2, out double x2) && int.TryParse(inputLabel, out int label))
+            {
+                dataSet.Add(new DataPoint(x1, x2, label));
+                Console.WriteLine("Точка додана!");
+            }
+            else
+            {
+                Console.WriteLine("Неправильне введення");
+            }
+        }
+
+        Console.WriteLine("Введення завершене.");
+        LinearClassifier classifier = new LinearClassifier(initialCoefficients: new double[] { 0.8, 0.4 }, initialBias: -1.2, learningRate: 0.15, maxEpochs: 1000);
+        classifier.Train(dataSet);
+
+        double slope = -classifier.coefficients[0] / classifier.coefficients[1];
+        double intercept = -classifier.bias / classifier.coefficients[1];
+
+        Console.WriteLine($"y = {slope}x + {intercept}".Replace(',', '.'));
+        Console.WriteLine("Введіть 'continue', щоб продовжити додавання точок:");
+        string cont = Console.ReadLine();
+        if (cont == "continue")
+            CreateDataSet(dataSet);
+
+        Console.ReadKey();
+        return dataSet;
+    }
+
+    static void Main(string[] args)
+    {
+        List<DataPoint> dataSet = CreateDataSet(null);
+    }
+}
---
+++ Neuro_1_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Neuro_1_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Neuro_1_1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("abdeaeeb-411e-4d6f-b3a7-eb5b30e9f4e5")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
